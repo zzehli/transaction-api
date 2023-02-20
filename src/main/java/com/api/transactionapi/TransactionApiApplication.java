042fcc0393a99acd2e233c244934f36b5d4ab492
jae <albertforus@gmail.com>
2023-02-20 00:41:31 -0500

    set up Spring Boot and Intellij

---
+++ src/main/java/com/api/transactionapi/TransactionApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.transactionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionApiApplication.class, args);
+	}
+
+}
