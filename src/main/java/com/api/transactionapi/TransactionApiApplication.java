e6e7cf1143c362fd2fa1a2d0c03980d92b911be7
jae <albertforus@gmail.com>
2023-06-22 23:54:47 -0400

    Authentica JWT token

--- src/main/java/com/api/transactionapi/TransactionApiApplication.java
+++ src/main/java/com/api/transactionapi/TransactionApiApplication.java
@@ -1,7 +1,10 @@
 package com.api.transactionapi;
 
+import com.api.transactionapi.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TransactionApiApplication {
@@ -10,4 +13,12 @@ public class TransactionApiApplication {
 		SpringApplication.run(TransactionApiApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+	}
 }
